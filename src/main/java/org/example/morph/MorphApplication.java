fb37499107d1047684934088abc5415fb211b243
gwangeunahn <gwangeunahn@hangdong.ac.kr>
2025-07-26 19:24:51 +0900

    Day 1-1
    init

---
+++ src/main/java/org/example/morph/MorphApplication.java
@@ -0,0 +1,13 @@
+package org.example.morph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/morph/MorphApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.morph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
