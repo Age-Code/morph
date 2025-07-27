422f90b3a95116b09c996ad87677abe5584383fb
gwangeunahn <gwangeunahn@hangdong.ac.kr>
2025-07-27 11:40:02 +0900

    Day 1-1
    Inited
    User added
    security added

--- src/main/java/org/example/morph/config/CorsFilterConfiguration.java
+++ src/main/java/org/example/morph/config/CorsFilterConfiguration.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 //헤더에 담을 내용도 확인!!
 @Configuration
 public class CorsFilterConfiguration {
+
    @Bean
    public CorsFilter corsFilter() {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -24,4 +25,5 @@ public class CorsFilterConfiguration {
       source.registerCorsConfiguration("/api/**", config);
       return new CorsFilter(source);
    }
+
 }
