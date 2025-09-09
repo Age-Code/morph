fe721a24547e9d417e8b7aafa773de157eab22bb
gwang <ahngwangeun@gmail.com>
2025-09-10 00:53:27 +0900

    Day 22-1

--- src/main/java/org/example/morph/repository/AnonCommentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.morph.repository;
-
-import org.example.morph.domain.AnonComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnonCommentRepository extends JpaRepository<AnonComment, Long> {
-}
--- src/main/java/org/example/morph/repository/AnonPostRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.morph.repository;
-
-import org.example.morph.domain.AnonPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnonPostRepository extends JpaRepository<AnonPost, Long> {
-}
